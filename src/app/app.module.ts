56635e5087a65dfb6b9a5c22700156bb9dab3ea5
Mahabubur Rahaman <mahababurrahaman2014@gmail.com>
2024-08-25 23:16:54 +0600

    top  banner add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBannerComponent } from './components/top-banner/top-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBannerComponent
   ],
   imports: [
     BrowserModule,

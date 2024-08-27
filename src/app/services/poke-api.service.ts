429c172ca9706e423a3fd3189a0a594b40a3bcbf
Mahabubur Rahaman <mahababurrahaman2014@gmail.com>
2024-08-28 00:03:34 +0600

    Refactor PokeApiService to use HttpClient for API requests

--- src/app/services/poke-api.service.ts
+++ src/app/services/poke-api.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { forkJoin, Observable, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
+  private baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+   }
+
+   getPokemonDetails(page: number): Observable<any>{
+      const url = `${this.baseUrl}?limit=20&offset=${page * 20}`;
+      return this.fetchPokemonList(url).pipe(
+        switchMap((result)=>forkJoin(
+          result.results.map((pokemon: any) => this.fetchIndividualPokemonDetailsByUrl(pokemon.url))
+        ))
+      )
+   }
+
+   fetchPokemonList(url: string): Observable<any>{
+      return this.http.get(url);
+   }
+
+   fetchIndividualPokemonDetailsByName(name: string): Observable<any> {
+    const url = `${this.baseUrl}/${name}`;
+    return this.fetchIndividualPokemonDetailsByUrl(url);
+  }
+
+  fetchIndividualPokemonDetailsByUrl(url: string): Observable<any> {
+    return this.http.get(url);
+  }
 }

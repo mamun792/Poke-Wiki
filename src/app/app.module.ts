704752716bde4abe71b22450fb11f32f85059ecd
Mahabubur Rahaman <mahababurrahaman2014@gmail.com>
2024-09-02 00:49:49 +0600

    chore: Add HttpClientModule for API requests and update routing for Pokemon detail page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopBannerComponent } from './components/top-banner/top-banner.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
-import { DetailsPageComponent } from './components/details-page/details-page.component';
+import { TopBannerComponent } from './components/top-banner/top-banner.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { DetailsPageComponent } from './components/details-page/details-page.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/details-page/details-page.component.ts
+++ src/app/components/details-page/details-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from '../../services/poke-api.service';
 @Component({
   selector: 'app-details-page',
   templateUrl: './details-page.component.html',
   styleUrl: './details-page.component.scss'
 })
-export class DetailsPageComponent {
+export class DetailsPageComponent implements OnInit {
+   constructor(private pokeApiService: PokeApiService) {
+
+   }
+
+    ngOnInit(): void {
+      this.pokeApiService.getPokemonDetails(1).subscribe({
+        next: (result) => {
+          console.log(result);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
 
 }
